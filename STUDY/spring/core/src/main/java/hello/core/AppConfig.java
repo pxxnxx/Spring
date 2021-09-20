ee5c1c10e3fac3013e97462ecfe053181c1cc91d
pxxnxx <kmin9811@naver.com>
2021-09-20 20:20:20 +0900

    스프링 컨테이너 주입

--- STUDY/spring/core/src/main/java/hello/core/AppConfig.java
+++ STUDY/spring/core/src/main/java/hello/core/AppConfig.java
@@ -1,20 +1,39 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     // *생성자를 통해 주입, injection*
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(MemberRepository());
     }
 
+    // 리팩터링을 통해 가시성 향상
+    @Bean
+    public MemoryMemberRepository MemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(MemberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+//        return new FixDiscountPolicy();
     }
 }
