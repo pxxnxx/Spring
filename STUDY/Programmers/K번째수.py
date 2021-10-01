428dc29fe34ffe513921d947e28ef5067acab588
pxxnxx <76719828+pxxnxx@users.noreply.github.com>
2021-10-01 23:03:04 +0900

    Delete STUDY/Programmers directory

--- STUDY/Programmers/K번째수.py
+++
@@ -1,9 +0,0 @@
-def solution(array, commands):
-    answer = []
-    for i in range(len(commands)):
-        arr = []
-        for j in range(commands[i][0]-1,commands[i][1]):
-            arr.append(array[j])
-        arr = sorted(arr)
-        answer.append(arr[(commands[i][2]-1)])
-    return answer
