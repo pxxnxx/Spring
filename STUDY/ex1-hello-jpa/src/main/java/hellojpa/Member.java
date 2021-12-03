8e916964acaa811d986c80ac9e811b112ee137c3
pxxnxx <76719828+pxxnxx@users.noreply.github.com>
2021-12-04 02:13:31 +0900

    다대다

--- STUDY/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ STUDY/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,13 +1,14 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
 
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
-
     private Long id;
 
     @Column(name = "USERNAME")
@@ -16,6 +17,9 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
