589df34f4ffcf2904dca13b51f6d5004dfb75913
pxxnxx <76719828+pxxnxx@users.noreply.github.com>
2021-12-07 21:19:08 +0900

    상속관계 매핑

--- STUDY/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ STUDY/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -13,17 +13,19 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
 
-            em.persist(member);
+            Movie movie = new Movie();
+            movie.setDirector("A");
+            movie.setActor("BBB");
+            movie.setName("바람과함께사라지다");
+            movie.setPrice(10000);
+            em.persist(movie);
 
-            Team team = new Team();
-            team.setName("teamA");
-            // 멤버테이블 업데이트 해야 함
-            team.getMembers().add(member);
+            em.flush();
+            em.clear();
 
-            em.persist(team);
+            Movie findMovie = em.find(Movie.class, movie.getId());
+            System.out.println("findMovie = " + findMovie);
 
             tx.commit();
         } catch (Exception e) {
