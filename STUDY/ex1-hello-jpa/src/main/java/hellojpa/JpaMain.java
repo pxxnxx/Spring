ca275a2336478a6ae471d960fdc106991cf2f47b
pxxnxx <76719828+pxxnxx@users.noreply.github.com>
2021-12-14 00:48:39 +0900

    영속성 전이(CASCADE)와 고아 객체

--- STUDY/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ STUDY/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,27 +15,20 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team);
-            em.persist(member1);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
 
             em.flush();
             em.clear();
 
-            Member m = em.find(Member.class, member1.getId());
-
-            // Proxy 상태
-            System.out.println("m.team.class :" + m.getTeam().getClass());
-
-            System.out.println("=============");
-            // 실제 Entity
-            System.out.println(m.getTeam().getName());
-            System.out.println("=============");
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
 
             tx.commit();
         } catch (Exception e) {
