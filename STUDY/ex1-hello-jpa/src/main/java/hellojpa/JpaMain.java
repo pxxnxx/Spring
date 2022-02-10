05740704180f5747df1be979f9db58c1eee1bfb1
pxxnxx <76719828+pxxnxx@users.noreply.github.com>
2022-02-11 02:50:10 +0900

    기본 키 매핑

--- STUDY/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ STUDY/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -14,10 +14,10 @@ public class JpaMain {
 
         try {
             Member member = new Member();
-            member.setId(1L);
-            member.setUsername("A");
-            member.setRoleType(RoleType.ADMIN);
+            member.setId("ID_A");
+            member.setUsername("C");
 
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
--- STUDY/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ STUDY/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -7,33 +7,20 @@ import java.util.Date;
 public class Member {
 
     @Id
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
 
     @Column(name = "name")
     private String username;
 
-    private Integer age;
-
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob
-    private String description;
-
     public Member() {
     }
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -43,45 +30,5 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
