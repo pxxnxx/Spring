a83d75391dbe54356167390f19de1f7f79d2efdc
pxxnxx <76719828+pxxnxx@users.noreply.github.com>
2022-02-17 22:43:58 +0900

    실전 예제 2

---
+++ STUDY/ex1-hello-jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.Team;
+
+import javax.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Order order = new Order();
+            em.persist(order);
+
+//            order.addOrderItem(new OrderItem());
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            em.persist(orderItem);
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
